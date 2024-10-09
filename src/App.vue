1b1a1458da0daf086f42d27df926e3561ab0ce3a
Zoe Sadler-Jasmin <zoe13@zoes-laptop.lan>
2024-10-08 22:29:32 -0400

    setting up vue app and supabase

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import  { supabase } from  '@/lib/supabaseClient';
+</script>
+
+<template>
+  <main>
+    <RouterView />
+    <router-link to="/">Home</router-link> |
+    <router-link to="/projects">Projects</router-link> |
+    <router-link to="/project">Project (Singular)</router-link>
+  </main>
+</template>
---
+++ src/pages/projects/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import  {  useRoute } from  'vue-router';
+
+const route = useRoute();
+</script>
+
+<template>
+<h1>Single Project {{ route.params?.id }}</h1>
+</template>
